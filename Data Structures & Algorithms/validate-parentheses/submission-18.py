5533d437abeba683dc09774a1e80f6481b935ee6
Olaiwonismail <olaiwonismail@gmail.com>
2026-06-14 13:50:23 +0100

    Add: validate-parentheses - submission-18

---
+++ Data Structures & Algorithms/validate-parentheses/submission-18.py
@@ -0,0 +1,29 @@
+class Solution:
+    def isValid(self, s: str) -> bool:
+        b = {
+            '}':'{',
+            ']':'[',
+            ')':'('
+        }
+        l =[]
+        if len(s) <= 1:
+            return False
+        s = list(s)
+        for x,item in enumerate(s):
+            if item in b.values():
+                l.append(item)
+            if item in b.keys():
+                if len(l) > 0:
+                    if l[-1] == b[item]:
+                        l.pop()
+                        if len(l) == 0 and x == len(s)-1:
+                            return True
+                    else:
+                        return False
+                else:
+                    return False
+        return False
+        
+
+            
+            
