0a3a2509fa99eb7a7c187f3223e1c8fadeba189a
Olaiwonismail <olaiwonismail@gmail.com>
2026-09-01 10:38:41 +0100

    Add: is-anagram - submission-5

---
+++ Data Structures & Algorithms/is-anagram/submission-5.py
@@ -0,0 +1,10 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        h1 ={}
+        h2= {}
+        
+        for letter in s:
+            h1[letter] = h1.get(letter,0)+1
+        for letter in t:
+            h2[letter] = h2.get(letter,0)+1
+        return h2==h1
