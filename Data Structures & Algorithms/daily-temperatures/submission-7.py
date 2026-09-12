7f8cf0322fd5faba882b64ed62ae40d811d27884
Olaiwonismail <olaiwonismail@gmail.com>
2026-09-12 18:53:33 +0100

    Add: daily-temperatures - submission-7

---
+++ Data Structures & Algorithms/daily-temperatures/submission-7.py
@@ -0,0 +1,34 @@
+class Solution:
+    def dailyTemperatures(self, temperatures: List[int]) -> List[int]:
+        l = len(temperatures)
+        t = temperatures
+        output = [0]*l
+        stack = []
+        # m ={}
+        # for n in range(0,l):
+        #     m[temp[n]] = n
+        
+        for n in range(0,l):
+            item  = t[n]
+            if not stack:
+                stack = [[item,n]]
+                continue
+            if item <= stack[-1][0]:
+                stack.append([item,n])
+               
+            else:
+                while item > stack[-1][0]:
+                    output[stack[-1][1]] =  n - stack[-1][1]
+                    stack.pop()
+                    if not stack:
+                        break
+                stack.append([item,n])
+                    
+        return output
+
+
+            
+
+            
+
+        
